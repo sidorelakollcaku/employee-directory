01225547c54187a3c836e15ef4170fc5dc5bf591
sidorela Kollcaku <sidorelakollcaku@gmail.com>
2020-10-30 13:35:20 -0400

    Updates

---
+++ precache-manifest.d250cddb609a8eb78ec0ede1e5e710c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d21829ba9dbce3492f616f4aa14105c",
+    "url": "/sidorelakollcaku/employee-directory/index.html"
+  },
+  {
+    "revision": "2ddc464f3172f472bf07",
+    "url": "/sidorelakollcaku/employee-directory/static/css/main.730cc9ac.chunk.css"
+  },
+  {
+    "revision": "dbc59f43f6b7bca6794b",
+    "url": "/sidorelakollcaku/employee-directory/static/js/2.b3c947f5.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/sidorelakollcaku/employee-directory/static/js/2.b3c947f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ddc464f3172f472bf07",
+    "url": "/sidorelakollcaku/employee-directory/static/js/main.1f38c4be.chunk.js"
+  },
+  {
+    "revision": "e92275ad61b3cb05dfdb",
+    "url": "/sidorelakollcaku/employee-directory/static/js/runtime-main.15242f37.js"
+  }
+]);
